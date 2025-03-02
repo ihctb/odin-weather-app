b9c36781ab14819981c68793515c4cd888ce786e
ihctb <mtrpenudleua@gmail.com>
2025-03-02 11:44:25 +0300

    fix API call

--- src/js/api.js
+++ src/js/api.js
@@ -37,7 +37,7 @@ const findCityByName = async (cityName) => {
     try {
         const [response] = await (
             await fetch(
-                `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+                `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
             )
         ).json();
 
